ee8de228a52ef171584749da3d9e70918bdded40
mai-cs50 <maimohamed.1992@yahoo.com>
2024-10-19 03:00:35 -0700

    k

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,15 @@
+-- Creates a table users with below attributes
+-- id, integer, never null, auto increment and primary key
+-- email, string (255 characters), never null and unique
+-- name, string (255 characters)
+-- If the table already exists, your script should not fail
+
+DELIMITER $$ ;
+CREATE TRIGGER resets_valid_email BEFOR UPDATE ON users
+FOR EACH ROW
+BEGIN
+	IF NEW.email != OLD.email THEN
+		SET NEW.valid_email = 0;
+	END IF;
+END;
+DELIMITER;
