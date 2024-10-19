47330d1c20655e35931e080d0753814a55241efe
mai-cs50 <maimohamed.1992@yahoo.com>
2024-10-19 02:46:48 -0700

    k

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -3,13 +3,13 @@ DROP TABLE IF EXISTS items;
 DROP TABLE IF EXISTS orders;
 
 CREATE TABLE IF NOT EXISTS items (
-	name VARCHAR(225) NOT NULL,
-	quantity int NOT NULL DEFAULT 10
+    name VARCHAR(225) NOT NULL,
+    quantity int NOT NULL DEFAULT 10
 );
 
 CREATE TABLE IF NOT EXISTS orders (
-	item_name VARCHAR(225) NOT NULL,
-	number int NOT NULL
+    item_name VARCHAR(225) NOT NULL,
+    number int NOT NULL
 );
 
 INSERT INTO items (name) VALUES ("apple"), ("pineapple"), ("pear")
