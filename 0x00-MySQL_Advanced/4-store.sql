270842f83676e5aa3d7f5dcd97ab706fd4fb6273
mai-cs50 <maimohamed.1992@yahoo.com>
2024-10-19 02:47:40 -0700

    k

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -3,12 +3,12 @@ DROP TABLE IF EXISTS items;
 DROP TABLE IF EXISTS orders;
 
 CREATE TABLE IF NOT EXISTS items (
-    name VARCHAR(225) NOT NULL,
+    name VARCHAR(255) NOT NULL,
     quantity int NOT NULL DEFAULT 10
 );
 
 CREATE TABLE IF NOT EXISTS orders (
-    item_name VARCHAR(225) NOT NULL,
+    item_name VARCHAR(255) NOT NULL,
     number int NOT NULL
 );
 
