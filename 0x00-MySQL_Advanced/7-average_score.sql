22a258c6ccc6c7a579533fbf73b61963d2f21367
mai-cs50 <maimohamed.1992@yahoo.com>
2024-10-19 03:36:02 -0700

    k

---
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,17 @@
+-- Creates a table users with below attributes
+-- id, integer, never null, auto increment and primary key
+-- email, string (255 characters), never null and unique
+-- name, string (255 characters)
+-- If the table already exists, your script should not fail
+
+DELIMITER $$ ;
+
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
+BEGIN
+	UPDATE users
+	SET
+	average_score = (SELECT AVG(score) FROM corrections WHERE corrections.user_id = user_id)
+	WHERE id = user_id
+END;$$
+DELIMITER;
